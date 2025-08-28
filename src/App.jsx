2c8b1424ffbbd3447d10e0f2f291533dda33aeb3
Dharminder Pal (CBT) <cbt.sd4@deepakfasteners.in>
2025-08-28 11:00:25 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import react from 'react'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+       <h1>Finance Tracker </h1>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
+ 
