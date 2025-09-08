5d5c2368f6ce22a9c71ecc9ffe39dcfc2f28891f
Dharminder Pal (CBT) <cbt.sd4@deepakfasteners.in>
2025-09-08 17:48:03 +0530

    new add

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,23 @@ import Signup from './component/SignupSignin'
 import { BrowserRouter,Route,Router,Routes } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
 import './App.css'
+import { ToastContainer, toast } from 'react-toastify';
 import firebase from 'firebase/compat/app'
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
+    <>
+    <ToastContainer/>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Header />} />
         <Route path="Dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
+    </>
   );
+  
 }
 
 
