6116726682d9cf4d497e8082dfe8d415c1b33da9
Dharminder Pal (CBT) <cbt.sd4@deepakfasteners.in>
2025-10-13 17:54:37 +0530

    10/13/2025

--- src/component/Cards/Cards.jsx
+++ src/component/Cards/Cards.jsx
@@ -5,12 +5,47 @@ import Button from "../Button";
 function index(){
 return(
 <div>
+
+{/* 
 <Row className="my-row">
+
 <Card    className="my-card"      title=" Current Balance">
     <p>₹0</p>
-<Button text= "Reset button"></Button>
+<Button text= "Reset button" blue={true}></Button>
     </Card>
+<Card    className="my-card"      title=" Current Balance">
+    <p>₹0</p>
+<Button text= "Reset button" blue={true}></Button>
+    </Card>
+<Card  className="my-card"      title=" Current Balance">
+    <p>₹0</p>
+<Button text= "Reset button" blue={true}></Button>    
+    </Card>
+</Row> */}
+<Row className="my-row">
+    <Card bordered={true} style={cardstyle} >
+        <h2>CurrentBalance</h2>
+        <p>₹{currentBalance}</p>
+       <div></div>
+
+    </Card>
+
+
+
+
 </Row>
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 </div>
