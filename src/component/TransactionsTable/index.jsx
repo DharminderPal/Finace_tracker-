126a1a9b25b94a3e56e3f1ac4fb19495c59ae019
dharminder pal <gauravpal786786@gmail.com>
2025-11-11 08:31:40 +0530

    today

--- src/component/TransactionsTable/index.jsx
+++ src/component/TransactionsTable/index.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { Table } from "antd";
-import { useState } from "react";
+import React, { useState } from "react";
+import { Table ,Select } from "antd";
 
 function TransactionsTable({ transactions }) {
   const [search, setSearch] = useState("");
+  const [typeFilter, setTypeFilter] = useState("");
   const columns = [
     {
       title: 'Name',
@@ -31,23 +31,45 @@ function TransactionsTable({ transactions }) {
       key: 'date',
     },
   ];
-let filtered =  transactions;
-if(search){
-  filtered = transactions.filter(item =>
-  item.name.toLowerCase().includes(search.toLowerCase()));
-
+  let filtered = transactions;
+  if (search) {
+    filtered = transactions.filter(item =>
+      item.name.toLowerCase().includes(search.toLowerCase())
+    );
   }
-}
   return (
+
+
+
     <div style={{ marginTop: 20 }}>
+
+      <input    value={search}
+      onChange={(e)=>setSearch(e.target.value)}
+       placeholder="search Transection "/>
+
+
+<Select
+
+class Level="large"
+onChange={(e)=>setTypeFilter(e)}
+value={typeFilter}
+placeholder="Filter by Type"
+allowClear
+>
+
+  <Option value="income">Income</Option>
+  <Option value="">All</Option>
+  <Option value="expense">Expense</Option>
+  
+</Select>
       <Table 
-        dataSource={transactions} 
+        dataSource={filtered} 
         columns={columns} 
         rowKey="id" 
         pagination={false} 
       />
     </div>
   );
-
+}
 
 export default TransactionsTable;
